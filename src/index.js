55aa649399cb0e61ef4dd67bd39e70b3550dd1bb
Jarrod Medrano <jmedran@gmail.com>
2021-01-02 15:24:44 -0700

    19 relational arrays

--- src/index.js
+++ src/index.js
@@ -62,6 +62,7 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
   
   type Post {
@@ -121,6 +122,13 @@ const resolvers = {
     author(parent, args, ctx, info) {
       return users.find((user) => {
         return user.id === parent.author;
+      });
+    },
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter((post) => {
+        return post.author === parent.id;
       });
     },
   },
