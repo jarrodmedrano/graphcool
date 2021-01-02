27c8f6802365858722b8bc40dcb9d649a277d278
Jarrod Medrano <jmedran@gmail.com>
2021-01-02 15:16:46 -0700

    18 - relationships

--- src/index.js
+++ src/index.js
@@ -29,18 +29,21 @@ const posts = [
     title: "Post1",
     body: "My post body",
     published: true,
+    author: "1",
   },
   {
     id: "2",
     title: "Post2",
     body: "My post body 2",
     published: true,
+    author: "1",
   },
   {
     id: "3",
     title: "Post3",
     body: "My post body",
     published: true,
+    author: "2",
   },
 ];
 
@@ -67,6 +70,7 @@ const typeDefs = `
     body: String!
     published: Boolean!
     type: String!
+    author: User!
   }
 `;
 
@@ -111,6 +115,13 @@ const resolvers = {
         published: true,
         type: "blog",
       };
+    },
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find((user) => {
+        return user.id === parent.author;
+      });
     },
   },
 };
