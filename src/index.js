c6b62e83712c112c6b54ea18720404a3a7d3fdac
Jarrod Medrano <jmedran@gmail.com>
2021-01-03 10:42:09 -0700

    25 create post mutation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { GraphQLServer } from "graphql-yoga";
-import { uuid } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 // String, Boolean, Int, Float
 
@@ -83,6 +83,7 @@ const typeDefs = `
 
   type Mutation {
     createUser(name: String!, email: String! age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
   }
 
   type User {
@@ -171,7 +172,6 @@ const resolvers = {
   },
   Mutation: {
     createUser(parent, args, ctx, info) {
-      console.log(args);
       const emailTaken = users.some((user) => {
         return user.email === args.email;
       });
@@ -181,7 +181,7 @@ const resolvers = {
       }
 
       const user = {
-        id: Math.random(),
+        id: uuidv4(),
         name: args.name,
         email: args.email,
         age: args.age,
@@ -190,6 +190,27 @@ const resolvers = {
       users.push(user);
 
       return user;
+    },
+    createPost(parent, args, ctx, info) {
+      const userExists = users.some((user) => {
+        return user.id === args.author;
+      });
+
+      if (!userExists) {
+        throw new Error("User not found");
+      }
+
+      const post = {
+        id: Math.random(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author,
+      };
+
+      posts.push(post);
+
+      return post;
     },
   },
   Post: {
