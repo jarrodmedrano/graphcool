e0e446cf1bfe11399d88d9cb32445593fe1ad20f
Jarrod Medrano <jmedran@gmail.com>
2021-01-03 09:15:01 -0700

    comment relationships

--- src/index.js
+++ src/index.js
@@ -83,6 +83,7 @@ const typeDefs = `
     email: String!
     age: Int
     posts: [Post!]!
+    comments: [Comment!]!
   }
   
   type Post {
@@ -176,6 +177,11 @@ const resolvers = {
     posts(parent, args, ctx, info) {
       return posts.filter((post) => {
         return post.author === parent.id;
+      });
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => {
+        return comment.author === parent.id;
       });
     },
   },
