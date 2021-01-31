1e0e59004204ce91352bee26d89e94f262f17c6b
Jarrod Medrano <jmedran@gmail.com>
2021-01-30 18:56:34 -0700

    update post

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -130,6 +130,34 @@ const Mutation = {
 
     return deletedComments[0];
   },
+
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
+
+    const post = db.posts.find((post) => {
+      console.log("post", post);
+      return post.id === id;
+    });
+
+    if (!post) {
+      throw new Error("Post not found");
+    }
+
+    if (typeof data.title === "string") {
+      post.title = data.title;
+    }
+
+    if (typeof data.body === "string") {
+      post.body = data.body;
+    }
+
+    if (typeof data.published === "boolean") {
+      post.published = data.published;
+    }
+
+    return post;
+  },
+
   createComment(parent, args, { db }, info) {
     const userExists = db.users.some((user) => {
       return user.id === args.data.author;
@@ -149,6 +177,20 @@ const Mutation = {
     };
 
     db.comments.push(comment);
+
+    return comment;
+  },
+  updateComment(parent, args, { db }, info) {
+    const { id, data } = args;
+    const comment = db.comments.find((comment) => comment.id === id);
+
+    if (!comment) {
+      throw new Error("Comment not found");
+    }
+
+    if (typeof data.text === "string") {
+      comment.text = data.text;
+    }
 
     return comment;
   },
